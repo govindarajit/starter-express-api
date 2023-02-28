a8943e53ae5fd19f32c9c2794611c56291cc8f0d
govindarajit <govindarajmrp@gmail.com>
2023-02-28 16:48:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ mongoose.connect(MONGO_URI, {
     .then(() => console.log('MongoDB connected!'))
     .catch(err => console.log(err));
 app.use('/api/posts', postsRoutes);
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
+// app.all('/', (req, res) => {
+//     console.log("Just got a request!")
+//     res.send('Yo!')
+// })
 app.listen(process.env.PORT || 3000)
