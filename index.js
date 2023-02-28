ba64e65abae08a6619c8a8c9b16b72f9036a1eea
govindarajit <govindarajmrp@gmail.com>
2023-02-28 16:42:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const express = require('express')
-const app = express()
+const app = express() 
+const mongoose = require('mongoose');
+const { MONGO_URI } = require('./config');
+
+const postsRoutes = require('./routes/api/posts_controller');
+
+//BodyParser Middleware 
+app.use(express.json());
+
+//connect to mongo
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true, 
+    useFindAndModify: false
+})
+    .then(() => console.log('MongoDB connected!'))
+    .catch(err => console.log(err));
+app.use('/api/posts', postsRoutes);
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
